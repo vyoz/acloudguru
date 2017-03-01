199c15554cd33d3bba67e011cc285d2d0aa3a572
Vincent Yang <vincentyangoz@gmail.com>
2017-03-01 17:50:52 +1100

    update php files
    update php files

--- connect.php
+++ connect.php
@@ -1,5 +1,4 @@
-<? 
-	$selected = mysql_select_db("$dbname", $dbhandle)  
-	or die ("Could not select database");
-	echo "echo "Connected to Dataabase - $dbame";  
+<?php
+	$selected = mysql_select_db("$dbname", $dbhandle) or die ("Could not select database");
+	echo "Connected to Dataabase - $dbame";  
 ?>
--- rds.sh
+++ rds.sh
@@ -5,4 +5,5 @@ chkconfig httpd on
 service httpd start
 echo "<?php phpinfo(); ?>" > /var/www/html/index.php
 cd /var/www/html/index
-wget https://s3-eu-west-1.amazonasw.com/acloudguru/connect.php
+wget https://raw.githubusercontent.com/vyoz/acloudguru/master/connect.php
+#wget https://s3-eu-west-1.amazonasw.com/acloudguru/connect.php
