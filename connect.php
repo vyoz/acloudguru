21d413cbc34731665aa5c72b5ac954a5620c1267
VTMEL\Vincent.Yang <vincent.yang@vedaleon.com>
2017-03-01 16:26:41 +1100

    script for rds lesson

---
+++ connect.php
@@ -0,0 +1,5 @@
+<? 
+	$selected = mysql_select_db("$dbname", $dbhandle)  
+	or die ("Could not select database");
+	echo "echo "Connected to Dataabase - $dbame";  
+?>
---
+++ rds.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+yum install httpd php php-mysql -y
+yum update -y
+chkconfig httpd on
+service httpd start
+echo "<?php phpinfo(); ?>" > /var/www/html/index.php
+cd /var/www/html/index
+wget https://s3-eu-west-1.amazonasw.com/acloudguru/connect.php
